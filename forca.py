7196e437fe020bfd788a13d6daef3cfe20e9abeb
PasqualiRafael <pasqualipiloto@gmail.com>
2021-03-04 16:52:48 -0300

    ajustando vidas

--- forca.py
+++ forca.py
@@ -6,9 +6,9 @@ from time import sleep
 
 def jogar():
 
-    show_title.ini(' Forca ')
+    show_title.ini(" Forca ")
 
-    def read_secret_word(file_name='secret_words.txt'):
+    def read_secret_word(file_name="secret_words.txt"):
         word_data = open(file_name, "r")
         list_data = word_data.read().lower().split()
         one_word = list_data[randint(0, (len(list_data) - 1))]
@@ -18,40 +18,42 @@ def jogar():
     secret_word = read_secret_word()
     victory = False
     die = False
-    life = 2
-    word = list('_' * len(secret_word))
-    print('A palavra tem {}{}{} letras.\n'.format(c["red"], len(secret_word), c["close"]))
+    life = 3
+    word = list("_" * len(secret_word))
+    print(
+        "A palavra tem {}{}{} letras.\n".format(c["red"], len(secret_word), c["close"])
+    )
 
     while not victory and not die:
 
         def msg_input():
-            print('')
-            print('Voce tem {}{}{} vidas.'.format(c["green"], life, c["close"]))
-            _letter = str(input(f'Escolha uma letra.\n').lower().strip())
+            print("")
+            print("Voce tem {}{}{} vidas.".format(c["green"], life, c["close"]))
+            _letter = str(input(f"Escolha uma letra.\n").lower().strip())
             return _letter
 
         def find_letter_word():
             for i, v in enumerate(secret_word):
                 sleep(0.2)
                 if v != letter:
-                    print(f'{word[i]}', end='')
+                    print(f"{word[i]}", end="")
                 elif v == letter:
-                    print(v, end='')
+                    print(v, end="")
                     word[i] = letter
 
         letter = msg_input()
         find_letter_word()
-        temp_join = ''.join(word)
+        temp_join = "".join(word)
 
         if letter not in secret_word:
             life -= 1
 
-        if life == -1:
-            print('\nVoce Perdeu!')
+        if life == 0:
+            print("\nVoce Perdeu!")
             die = True
 
         if temp_join == secret_word:
-            print('\nVoce venceu!')
+            print("\nVoce venceu!")
             victory = True
 
     show_title.close()
@@ -59,4 +61,3 @@ def jogar():
 
 if __name__ == "__main__":
     jogar()
-
--- main.py
+++ main.py
@@ -7,23 +7,23 @@ import forca
 
 def menu():
 
-    print(f'Choose your game: ')
-    choose_game = '0'
-    while choose_game not in '123':
-        choose_game = str(input(f'(1) Guess | (2) Jokenpo | (3) Forca.\n'))
-        if choose_game not in '123':
-            print(f'Essa opcao nao existe.\n')
+    print(f"Choose your game: ")
+    choose_game = "0"
+    while choose_game not in "123":
+        choose_game = str(input(f"(1) Guess | (2) Jokenpo | (3) Forca.\n"))
+        if choose_game not in "123":
+            print(f"Essa opcao nao existe.\n")
 
-    if choose_game == '1':
-        os.system('clear')
+    if choose_game == "1":
+        os.system("clear")
         adivinha.jogar()
-    elif choose_game == '2':
-        os.system('clear')
+    elif choose_game == "2":
+        os.system("clear")
         jokenpo.jogar()
-    elif choose_game == '3':
-        os.system('clear')
+    elif choose_game == "3":
+        os.system("clear")
         forca.jogar()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     menu()
